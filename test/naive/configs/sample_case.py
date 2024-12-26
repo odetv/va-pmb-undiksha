b78b7132ea1eb6c7ac59070babaf7b0030195982
odetv <abdiutama02@gmail.com>
2024-12-26 21:45:20 +0800

    fix: rechecking list qa and optimization ragas evaluation in rag adaptive

---
+++ test/naive/configs/sample_case.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+questions = pd.read_excel('test/naive/configs/list_qa.xlsx', sheet_name='QA', usecols='D')['QUESTION'].tolist()
+ground_truths = pd.read_excel('test/naive/configs/list_qa.xlsx', sheet_name='QA', usecols='E')['ANSWER'].tolist()
