b7afb6d57da43e2b31eb6d4e2d74efe37afa0827
odetv <abdiutama02@gmail.com>
2025-01-01 20:39:41 +0800

    fix: bug cache chat example and init firebase

--- app/chat.py
+++ app/chat.py
@@ -13,13 +13,12 @@ from dotenv import load_dotenv
 load_dotenv()
 STREAMLIT_KEY_ADMIN = os.getenv("STREAMLIT_KEY_ADMIN")
 st.set_page_config(page_title="VA PMB Undiksha", layout="wide", page_icon="public/images/logo.png")
-@st.cache_resource
 def init_firebase():
     cred = credentials.Certificate({
         "type": os.getenv("FIREBASE_TYPE"),
         "project_id": os.getenv("FIREBASE_PROJECT_ID"),
         "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"), # Berhati-hati dengan newlines di private key
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
         "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
         "client_id": os.getenv("FIREBASE_CLIENT_ID"),
         "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
